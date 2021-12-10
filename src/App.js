4c922cf5874315fc9e0cf3c9e09f96f312725cdb
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2021-12-10 13:13:13 +0200

    Crate PostWrittingCard Components and CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import PostWrittingCard from "./PostWrittingCard";
 
 function App() {
   return (
     <div>
-    
+    <PostWrittingCard />
     </div>
   );
 }
---
+++ src/PostWrittingCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const PostWrittingCard = ({change,submit}) => {
+   
+    return (
+        <div>
+            <form  onSubmit={submit}>
+            <div className="createPost">
+            
+                        <div className="icon" > 
+                            <i  className="far fa-user"></i>
+                        </div>
+                       <div>
+                            <input type="text" name="createPost" id="createPost" required onChange = {change}  placeholder="what's on your mind, ......?" />
+                       </div>
+                        <div>
+                            <input type="submit" value="Create post"/>  
+                        </div>
+                        
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default PostWrittingCard;
